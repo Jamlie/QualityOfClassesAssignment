ed84ade2eadb06863dcf9c733c6d5cbfa96e181d
Omar <88511754+Jamlee977@users.noreply.github.com>
2023-09-27 17:53:08 +0300

    Added a UniversityReporter class that reports the things needed, refactoring the university to do its tasks (single purpose)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import jam.com.Course;
 import jam.com.Student;
 import jam.com.University;
+import jam.com.UniversityReporter;
 
 public class Main {
     public static void main(String[] args) {
         University university = new University();
+        UniversityReporter.initializeUniversity(university);
 
         Student studentJamlie = new Student(977, "Jamlie", "jamlee977@outlook.com");
         Student studentOmar = new Student(420, "Omar", "omares977@outlook.com");
         Course introToProgramming1 = new Course("CS101", "Introduction to Programming 1", "Dr. Ahmed", 2);
         Course introToProgramming2 = new Course("CS102", "Introduction to Programming 2", "Dr. Ahmed", 2);
+
+        UniversityReporter.initializeStudent(studentJamlie);
+        UniversityReporter.initializeCourse(introToProgramming1);
 
         university.addStudent(studentJamlie);
         university.addStudent(studentOmar);
@@ -22,7 +27,7 @@ public class Main {
         university.registerStudentToCourse(studentOmar, introToProgramming2);
 
         System.out.println("After registering students to courses:");
-        university.generateReports();
+        UniversityReporter.generateReports();
 
         System.out.println("----------------------------------------");
 
@@ -30,6 +35,6 @@ public class Main {
         university.unregisterStudentFromCourse(studentOmar, introToProgramming2);
 
         System.out.println("After unregistering students from courses:");
-        university.generateReports();
+        UniversityReporter.generateReports();
     }
 }
---
+++ src/jam/com/UniversityReporter.java
@@ -0,0 +1,131 @@
+package jam.com;
+
+/**
+ * <h1>UniversityReporter</h1>
+ * <p>A UniversityReporter is used to generate reports off a given university via initializeUniversity</p>
+ */
+public class UniversityReporter {
+    private UniversityReporter() {}
+
+    private static University university;
+    private static Student student;
+    private static Course course;
+
+    /**
+     * <p>Initializes UniversityReporter.university</p>
+     * @param university The university
+     */
+    public static void initializeUniversity(University university) {
+        UniversityReporter.university = university;
+    }
+
+    /**
+     * <p>Initializes UniversityReporter.student</p>
+     * @param student The student
+     */
+    public static void initializeStudent(Student student) {
+        UniversityReporter.student = student;
+    }
+
+    /**
+     * <p>Initializes UniversityReporter.course</p>
+     * @param course The course
+     */
+    public static void initializeCourse(Course course) {
+        UniversityReporter.course = course;
+    }
+
+    /**
+     * <p>Checks if UniversityReporter.university is null</p>
+     * @return boolean
+     */
+    private static boolean isUniversityNull() {
+        return UniversityReporter.university == null;
+    }
+
+    /**
+     * <p>Checks if UniversityReporter.student is null</p>
+     * @return boolean
+     */
+    private static boolean isStudentNull() {
+        return UniversityReporter.student == null;
+    }
+
+    /**
+     * <p>Checks if UniversityReporter.course is null</p>
+     * @return boolean
+     */
+    private static boolean isCourseNull() {
+        return UniversityReporter.course == null;
+    }
+
+    /**
+     * <p>Reports (prints) all courses within the University using the <code>report</code> method that was given by the interface <code>Reportable</code></p>
+     */
+    public static void reportCourses() {
+        if (isUniversityNull()) {
+            System.out.println("University is not initialized. Use UniversityReporter.InitializeUniversity(University university) to initialize it.");
+            return;
+        }
+
+        System.out.println("Courses:");
+        System.out.print("[");
+        for (var course : university.getCourses()) {
+            course.report();
+            if (course != university.getCourses().get(university.getCourses().size() - 1)) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+        System.out.println("---------------------------------------------------");
+    }
+
+    /**
+     * <p>Reports (prints) all students who have the course</p>
+     */
+    public static void reportStudentsWithinTheCourse() {
+        if (isUniversityNull()) {
+            System.out.println("University is not initialized. Use UniversityReporter.InitializeUniversity(University university) to initialize it.");
+            return;
+        }
+
+        if (isCourseNull()) {
+            System.out.println("Course is not initialized. Use UniversityReporter.InitializeCourse(Course course) to initialize it.");
+            return;
+        }
+
+        System.out.println("Students within the course:");
+        System.out.println(course.getStudentsWithinTheCourse());
+        System.out.println("---------------------------------------------------");
+    }
+
+    /**
+     * <p>Reports (prints) all courses of the student</p>
+     */
+    public static void reportCoursesOfTheStudent() {
+        if (isUniversityNull()) {
+            System.out.println("University is not initialized. Use UniversityReporter.InitializeUniversity(University university) to initialize it.");
+            return;
+        }
+
+        if (isStudentNull()) {
+            System.out.println("Student is not initialized. Use UniversityReporter.InitializeStudent(Student student) to initialize it.");
+            return;
+        }
+
+        System.out.println("Courses of the student:");
+        System.out.println(student.getCourses());
+        System.out.println("---------------------------------------------------");
+    }
+
+    public static void generateReports() {
+        if (isUniversityNull()) {
+            System.out.println("University is not initialized. Use UniversityReporter.InitializeUniversity(University university) to initialize it.");
+            return;
+        }
+
+        reportCourses();
+        reportStudentsWithinTheCourse();
+        reportCoursesOfTheStudent();
+    }
+}
